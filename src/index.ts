361229bd9796fd7b9410f534b3dd8133493225af
Daniel Oguejiofor <oguejiofor.daniel@chigisoft.com>
2023-08-24 03:30:15 +0100

    fix: made an edit to the package.json file and stalled redis

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     app.listen(port, async () => {
-      await monitorRedisConnection();
+      // await monitorRedisConnection();
       await mongodb();
       logger.info(`NOTE APP Core Service [${process.env.NODE_ENV}] running on port: ${port}`);
     });
